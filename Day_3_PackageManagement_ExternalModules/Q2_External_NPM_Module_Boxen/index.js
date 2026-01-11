bb985b974a2d555ecb72428c49c3efc7ed686d62
Mahalaxmi-Komuravelly <mahalaxmikomuravelly@gmail.com>
2026-01-11 19:14:40 +0530

    Add boxen package to display messages in different box styles

---
+++ Day_3_PackageManagement_ExternalModules/Q2_External_NPM_Module_Boxen/index.js
@@ -0,0 +1,5 @@
+import boxen from "boxen";
+
+console.log(boxen("I am using my first external module!",{title: "Hurray!!!",titleAlignment:"center",borderStyle:"classic",borderColor:"greenBright"}));
+console.log(boxen("I am using my first external module!",{title:"Hurray!!!",titleAlignment:"center",borderStyle:"singleDouble",borderColor:"cyan",backgroundColor:"magenta"}));
+console.log(boxen("unicorns love rainbows",{title:"Hurray!!!",titleAlignment:"center",borderStyle:"round",borderColor:"magenta"}));
