d77c2bb7087ae749db00dee24f9e09bda57b0bb5
Mahalaxmi-Komuravelly <mahalaxmikomuravelly@gmail.com>
2026-01-21 20:31:17 +0530

    final commit

--- Coding_Contest/routes/analytics.routes.js
+++ Coding_Contest/routes/analytics.routes.js
@@ -2,3 +2,54 @@ import express from "express"
 
 export const AnalyticsRoutes = express.Router();
 
+import { readData,writeData } from "./orders.routes.js";
+AnalyticsRoutes.get("/allorders",(req,res)=>{
+    const data = readData();
+    if (!data) {
+        return res.status(500).json({ message: "Data Unavailable" })
+    }
+    let count = 0
+    data.orders.forEach((order)=>{
+        count++
+    })
+    res.status(200).json({message:"All orders list and count of orders",count,orders:data.orders})
+})
+AnalyticsRoutes.get("/cancelled-orders",(req,res)=>{
+    const data = readData();
+    if (!data) {
+        return res.status(500).json({ message: "Data Unavailable" })
+    }
+    const cancelledOrders = data.orders.filter((o)=>o.status === "cancelled");
+    if(cancelledOrders.length===0){
+        return res.status(400).json({message:"There are no cancelled orders"})
+    }
+    else{
+    res.status(200).json({message:"All cancelled orders",cancelledOrders})
+
+    }
+})
+
+AnalyticsRoutes.get("/shipped",(req,res)=>{
+    const data = readData();
+    if (!data) {
+        return res.status(500).json({ message: "Data Unavailable" })
+    }
+    const shippedOrders = data.orders.filter((o)=>o.status === "shipped");
+    if(shippedOrders.length===0){
+        return res.status(400).json({message:"There are no shipped orders"})
+    }
+    let count = 0
+    shippedOrders.forEach((order)=>{
+        count++
+    })
+    res.status(200).json({message:"All shipped orders with count",count,shippedOrders})
+})
+AnalyticsRoutes.get("/total-revenue/:productId",(req,res)=>{
+    const data = readData();
+    if (!data) {
+        return res.status(500).json({ message: "Data Unavailable" })
+    }
+    const id = Number(req.params.productId);
+
+    const  requiredOrders = data.orders.filter((o)=>o.id!==id);
+})
--- Coding_Contest/routes/orders.routes.js
+++ Coding_Contest/routes/orders.routes.js
@@ -17,72 +17,104 @@ export function writeData(data) {
 
 // create order
 
-OrdersRoutes.get("/",(req,res)=>{
+OrdersRoutes.get("/", (req, res) => {
     const data = readData();
-    if(!data){
-        return res.status(500).json({message:"Data Unavailable"})
+    if (!data) {
+        return res.status(500).json({ message: "Data Unavailable" })
     }
-    res.status(200).json({message:"All Orders List",orders:data.orders})
+    res.status(200).json({ message: "All Orders List", orders: data.orders })
 })
 
-OrdersRoutes.post("/",(req,res)=>{
+OrdersRoutes.post("/", (req, res) => {
     const data = readData();
-    if(!data){
-        return res.status(500).json({message:"Data Unavailable"})
+    if (!data) {
+        return res.status(500).json({ message: "Data Unavailable" })
     }
-    const {productId,quantity} = req.body || {}
-    if(!productId || !quantity){
-        return res.status(404).json({message:"ProductId and quantity are required"})
+    const { productId, quantity } = req.body || {}
+    if (!productId || !quantity) {
+        return res.status(404).json({ message: "ProductId and quantity are required" })
     }
-    const product = data.products.find((p)=>p.id === productId);
-    if(!product){
-        return res.status(404).json({message:"Product Not found"})
+    const product = data.products.find((p) => p.id === productId);
+    if (!product) {
+        return res.status(404).json({ message: "Product Not found" })
     }
-    if(product.stock === 0){
-        return res.status(400).json({message:"Insufficient stock"})
+    if (product.stock === 0) {
+        return res.status(400).json({ message: "Insufficient stock" })
     }
-    if(quantity > product.stock){
-        return res.status(400).json({message:"Quantity is more than product stock"})
+    if (quantity > product.stock) {
+        return res.status(400).json({ message: "Quantity is more than product stock" })
     }
     const totalAmount = product.price * quantity
     const newOrder = {
-        id:data.orders.length + 1,
+        id: data.orders.length + 1,
         productId,
         quantity,
         totalAmount,
-        status:"placed",
-        createdAt:new Date().toISOString().slice(0,10)
+        status: "placed",
+        createdAt: new Date().toISOString().slice(0, 10)
     }
     product.stock -= quantity;
     data.orders.push(newOrder);
     writeData(data);
-    res.status(201).json({message:"Order cretaed",order:newOrder})
+    res.status(201).json({ message: "Order cretaed", order: newOrder })
 })
 
-OrdersRoutes.delete("/:orderId",(req,res)=>{
+OrdersRoutes.delete("/:orderId", (req, res) => {
     const data = readData();
-    if(!data){
-        return res.status(500).json({message:"Data Unavailable"})
+    if (!data) {
+        return res.status(500).json({ message: "Data Unavailable" })
     }
     const id = Number(req.params.orderId);
-    const order = data.orders.find((o)=>o.id  === id);
-    console.log(order)
-    if(!order){
-        return res.status(404).json({message:"Order Not found"})
+    const order = data.orders.find((o) => o.id === id);
+    if (!order) {
+        return res.status(404).json({ message: "Order Not found" })
     }
-    if(order.status === "cancelled"){
-        return res.status(400).json({message:"Order is already cancelled"})
+    if (order.status === "cancelled") {
+        return res.status(400).json({ message: "Order is already cancelled" })
     }
-    const currentDate = new Date().toISOString().slice(0,10);
-    console.log(order.createdAt)
-    if(order.createdAt === currentDate){
-        order.status ="cancelled"
-    }
-    const product = data.products.find((p)=>p.id === order.productId)
-    if(!product){
+    const currentDate = new Date().toISOString().slice(0, 10);
+    const product = data.products.find((p) => p.id === order.productId)
+    if (!product) {
         return res.status(404).json({ message: "Product not found" })
     }
+    if (order.createdAt !== currentDate) {
+        return res.status(400).json({ message: "The order is cancelled only on the same day" })
+    }
+    order.status = "cancelled"
     product.stock += order.quantity;
     writeData(data);
-    res.status(200).json({message:"Order status changed to cancelled",order})
+    res.status(200).json({ message: "Order status changed to cancelled", order })
+})
+
+OrdersRoutes.patch("/change-status/:orderId", (req, res) => {
+    const data = readData();
+    if (!data) {
+        return res.status(500).json({ message: "Data Unavailable" })
+    }
+    const id = Number(req.params.orderId);
+    const order = data.orders.find((o)=>o.id === id);
+    if(!order){
+        return res.status(404).json({message:"Order not found"})
+    }
+    const {status} = req.body || {};
+    if(!status){
+        return res.status(400).json({message:"status required"})
+    }
+    if(status === "placed"){
+        return res.status(400).json({message:"Status is already placed"})
+    }
+    if(order.status === "cancelled" || order.status === "delivered"){
+        return res.status(400).json({message:"Status can't be changed"})
+    }
+   if (order.status === "shipped" && status !== "delivered") {
+        return res.status(400).json({ message: "Invalid status transition" });
+    }
+
+    order.status = status;
+    writeData(data);
+
+    return res.status(200).json({
+        message: "Order status updated successfully",
+        order
+    });
 })
